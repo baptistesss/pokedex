b98cedf7540cc44320707c6c9913b1e8591d3a25
Baptiste cazenave <paulcazenave@air-de-paul.home>
2024-03-26 19:32:47 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import PokemonCards from './components/PokemonCards'
+
+function App() {
+    return (
+    <div>
+      <PokemonCards/>
+    </div>
+    
+  );
+}
+
+export default App;
