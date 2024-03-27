40a31c1759c3c62ea5ee1b7b1e6deb8deec3bff9
Baptiste cazenave <paulcazenave@air-de-paul.home>
2024-03-27 13:04:16 +0100

    new commit

--- src/components/PokemonCards.jsx
+++ src/components/PokemonCards.jsx
@@ -1,5 +1,5 @@
  
- function PokemonCards (){
+ function PokemonCards(){
 return <figure>
 
       <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
